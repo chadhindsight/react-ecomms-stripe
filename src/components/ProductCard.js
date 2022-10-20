5de63d6af4b57d39f6ad9ee748ef2f600a49204d
chad hinds <chadrickj8@gmail.com>
2022-10-20 14:16:25 -0400

    destructure product from props

---
+++ src/components/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Card, Button, Form, Row, Col } from 'react-bootstrap'
+
+const ProductCard = (props) => {
+    // destructure product from props
+    const { product } = props;
+
+    return (
+        <Card>
+
+        </Card>
+    );
+};
+
+export default ProductCard;
--- src/pages/Store.js
+++ src/pages/Store.js
@@ -1,7 +1,19 @@
 
+import { Row, Col } from 'react-bootstrap';
+import { productsArray } from '../productsStore';
+
 function Success() {
     return (
-        <h1>Welcome to our store</h1>
+        <>
+            <h1 align="center" className='p-3'>Welcome to our store</h1>
+            <Row xs={1} md={3} className="g-4">
+                {productsArray.map((item, idx) => (
+                    <Col align="center" key={idx}>
+                        <h1>{item.title}</h1>
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
 
