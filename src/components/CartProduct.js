820039f11742756d97939afab9f26c66513fa23c
chad hinds <chadrickj8@gmail.com>
2022-10-24 14:49:48 -0400

    Fix comment typo

--- src/components/CartProduct.js
+++ src/components/CartProduct.js
@@ -4,8 +4,8 @@ import { useContext } from "react";
 import { getProductData } from "../productsStore";
 
 function CartProduct(props) {
-    const cart = useContext(CartContext);
     // Product Info
+    const cart = useContext(CartContext);
     const id = props.id;
     const quantity = props.quantity;
     const productData = getProductData(id);
